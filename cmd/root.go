f0651b2311098812c177d07fcca57ff9052aae58
Manapia <44744671+Manapia@users.noreply.github.com>
2020-06-05 10:42:04 +0900

    Add the download job builder and its options

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,28 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"log"
 	"ochibahiroi-cli/downloader"
+	"os"
 )
 
+type rootOption struct {
+	parallels     int
+	showProgress  bool
+	dryRun        bool
+	url           string
+	numberStart   int
+	numberEnd     int
+	step          int
+	resetNumber   bool
+	outputPath    string
+	makeOutputDir bool
+}
+
 var rootCmd *cobra.Command
-var rootOption = &downloader.DownloadOption{}
+var rootOptions = &rootOption{}
 
 func init() {
 	rootCmd = &cobra.Command{
@@ -16,8 +31,16 @@ func init() {
 		Run:   rootRun,
 	}
 
-	rootCmd.Flags().IntVarP(&rootOption.Parallels, "parallels", "m", 2, "Number of concurrent downloads")
-	rootCmd.Flags().BoolVarP(&rootOption.ShowProgress, "progress", "p", true, "Show progress")
+	rootCmd.Flags().IntVarP(&rootOptions.parallels, "parallels", "m", 2, "Number of concurrent downloads.")
+	rootCmd.Flags().BoolVarP(&rootOptions.showProgress, "progress", "p", true, "Show progress bars.")
+	rootCmd.Flags().BoolVarP(&rootOptions.dryRun, "dry-run", "d", false, "Show a list of files to download.")
+	rootCmd.Flags().StringVarP(&rootOptions.url, "url", "u", "", "URL of the download source.")
+	rootCmd.Flags().IntVar(&rootOptions.numberStart, "start", 0, "The first number in the sequence.")
+	rootCmd.Flags().IntVar(&rootOptions.numberEnd, "end", 0, "The last number in the sequence.")
+	rootCmd.Flags().IntVar(&rootOptions.step, "step", 1, "Number of steps in a sequence.")
+	rootCmd.Flags().BoolVarP(&rootOptions.resetNumber, "reset-number", "r", false, "Use numbers that start with 1 instead of the original filename.")
+	rootCmd.Flags().StringVarP(&rootOptions.outputPath, "output-path", "o", "./", "The output destination of the downloaded files.")
+	rootCmd.Flags().BoolVar(&rootOptions.makeOutputDir, "make-output", false, "If the destination folder does not exist, it will be created.")
 }
 
 func Execute() {
@@ -28,9 +51,45 @@ func Execute() {
 }
 
 func rootRun(cmd *cobra.Command, args []string) {
-	jobs := make([]*downloader.Job, 0, 10)
+	builder := downloader.JobListBuilder{}
+	builder.SetUrl(rootOptions.url)
+	builder.SetSavePath(rootOptions.outputPath)
+	builder.SetStart(rootOptions.numberStart)
+	builder.SetEnd(rootOptions.numberEnd)
+	builder.SetStep(rootOptions.step)
+	builder.SetUserIncrementalCount(rootOptions.resetNumber)
 
-	option := *rootOption
+	jobs, err := builder.Build()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if rootOptions.dryRun {
+		showDryRun(jobs)
+		return
+	}
+
+	outputPathStat, err := os.Stat(rootOptions.outputPath)
+	if err != nil {
+		if rootOptions.makeOutputDir {
+			if err := os.MkdirAll(rootOptions.outputPath, 0666); err != nil {
+				log.Fatalln(err)
+			}
+		}
+	} else if !outputPathStat.IsDir() {
+		log.Fatalf("output path %s is not directory", rootOptions.outputPath)
+	}
+
+	option := downloader.DownloadOption{
+		Parallels:    rootOptions.parallels,
+		ShowProgress: rootOptions.showProgress,
+	}
 
 	downloader.Run(jobs, option)
+}
+
+func showDryRun(jobs []*downloader.Job) {
+	for _, job := range jobs {
+		fmt.Printf("%s => %s\n", job.Url, job.SavePath)
+	}
 }
--- downloader/job.go
+++ downloader/job.go
@@ -1,7 +1,86 @@
 package downloader
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
 type Job struct {
 	Url string
 
 	SavePath string
+}
+
+type JobListBuilder struct {
+	url string
+
+	savePath string
+
+	startNumber int
+
+	endNumber int
+
+	step int
+
+	useIncrementalCount bool
+}
+
+func (b *JobListBuilder) SetUrl(url string) {
+	b.url = url
+}
+
+func (b *JobListBuilder) SetSavePath(path string) {
+	strings.TrimRight(path, string(os.PathSeparator))
+	b.savePath = path
+}
+
+func (b *JobListBuilder) SetStart(number int) {
+	b.startNumber = number
+}
+
+func (b *JobListBuilder) SetEnd(number int) {
+	b.endNumber = number
+}
+
+func (b *JobListBuilder) SetStep(step int) {
+	b.step = step
+}
+
+func (b *JobListBuilder) SetUserIncrementalCount(value bool) {
+	b.useIncrementalCount = value
+}
+
+func (b *JobListBuilder) Build() ([]*Job, error) {
+	jobs := make([]*Job, 0, 10)
+
+	if b.savePath == "" {
+		current, err := filepath.Abs(".")
+		if err != nil {
+			b.savePath = current
+		}
+	}
+
+	count := 1
+	if b.url != "" {
+		for i := b.startNumber; i <= b.endNumber; i += b.step {
+			url := fmt.Sprintf(b.url, i)
+
+			savePath := filepath.Join(b.savePath, filepath.Base(url))
+			if b.useIncrementalCount {
+				savePath = filepath.Join(b.savePath, strconv.Itoa(count)+filepath.Ext(url))
+			}
+
+			jobs = append(jobs, &Job{
+				Url:      url,
+				SavePath: savePath,
+			})
+
+			count++
+		}
+	}
+
+	return jobs, nil
 }
