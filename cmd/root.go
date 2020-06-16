b991441f8c1223da7a58cfb415bbd76eb50763a9
Manapia <44744671+Manapia@users.noreply.github.com>
2020-06-17 01:36:27 +0900

    1.1.2

--- cmd/root.go
+++ cmd/root.go
@@ -37,7 +37,7 @@ func init() {
 		Run:   rootRun,
 	}
 
-	rootCmd.Version = "1.1.1"
+	rootCmd.Version = "1.1.2"
 
 	rootCmd.Flags().IntVarP(&rootOptions.parallels, "parallels", "m", 2, "Number of concurrent downloads.")
 	rootCmd.Flags().BoolVarP(&rootOptions.showProgress, "progress", "p", true, "Show progress bars.")
